f961988a1c5cccb44b52cf3248f437205c17000d
Lekbear <Lek36820@gmail.com>
2024-08-22 13:40:17 +0300

    Lesson15: refactoring ResumeServlet

--- src/com/basejava/webapp/web/ResumeServlet.java
+++ src/com/basejava/webapp/web/ResumeServlet.java
@@ -2,15 +2,25 @@ package com.basejava.webapp.web;
 
 import com.basejava.webapp.Config;
 import com.basejava.webapp.model.Resume;
+import com.basejava.webapp.storage.Storage;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
 
 public class ResumeServlet extends HttpServlet {
+    private Storage storage;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        storage = Config.getInstance().getStorage();
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
@@ -26,8 +36,7 @@ public class ResumeServlet extends HttpServlet {
         out.println("<th>fullName</<th>");
         out.println("</tr>");
 
-        List<Resume> resumeList = Config.getInstance().getStorage().getAllSorted();
-        for (Resume resume : resumeList) {
+        for (Resume resume : storage.getAllSorted()) {
             out.println("<tr>");
             out.println("<td>" + resume.getUuid() + "</td>");
             out.println("<td>" + resume.getFullName() + "</td>");
